a3e29ce5b5cf5c84031c97249c5c91c4a8b630ed
er-gh <78344075+er-gh@users.noreply.github.com>
2023-04-26 19:42:38 +0700

    2

--- stocks.py
+++ stocks.py
@@ -6,7 +6,7 @@ from dateutil.relativedelta import relativedelta
 import json
 
 
-def load_json():
+def load_json() -> dict:
         with open('stocks.json') as fp:
             return json.load(fp)
         
@@ -17,13 +17,85 @@ def save_json(json_file: dict) -> None:
 
 
 def window():
-    root = tk.Tk()
-    root.title('Stocks')
-    root.geometry('200x200')
+    
 
-    money = 0
+    def entryEdited(event):
+        try:
+            nonlocal input_money
+            input_money = float(event.get())
+            label['text'] = f'Ввод: {input_money}'
+            remove_btn['text'] = f'Вычесть {input_money}'
+        except ValueError:
+            label['text'] = 'Ввод:'
+            remove_btn['text'] = 'Вычесть '
+
+    
+    def calc_weeks(date_start, date):
+        nonlocal weeks
+        weeks = relativedelta(date, date_start)
+        temp_weeks = datetime.now()
+        weeks = temp_weeks - (temp_weeks - weeks)
+        weeks = weeks.days // 7
+        print(weeks)
+    
+
+    def calc_money(json_file: dict) -> None:
+        nonlocal money
+        nonlocal input_money
+        nonlocal money_log
+        money = input_money
+        money += json_file['money']
+        money = round(money, 3)
+        json_file['money'] = money
+        json_file['date'] = f'{date.year}.{date.month}.{date.day}'
+        money_log['add'] += input_money
+
+    
+    def save(json_file: dict, func) -> None:
+        func(json_file)
+        label_from_json['text'] = f"Из файла: {json_file['money']}"
+        save_json(json_file)
+        print(json_file)
+
+    
+    def remove(json_file: dict) -> None:
+        if json_file['money'] <= input_money: json_file['money'] = 0.0
+        else: json_file['money'] -= input_money
+        money_log['remove'] += input_money
+
+    
+    def calc_percent(json_file, date_start):
+        nonlocal money
+        nonlocal weeks
+        if weeks > 0:
+            for _ in range(weeks): money += (money * 0.1)
+            money = round(money, 3)
+            date_start += relativedelta(weeks=+weeks)
+            json_file['date_start'] = f'{date_start.year}.{date_start.month}.{date_start.day}'
+            json_file['money'] = money
+        label_calc_perc['text'] = f'Процент за {weeks} недель: {round(money * 0.1 * weeks, 3)}'
+        label_from_json['text'] = f"Из файла: {json_file['money']}"
+        print(json_file)
+        save_json(json_file)
+
+
+    def report():
+        with open('report.txt', 'a+', encoding='utf-8') as fp:
+            fp.writelines(f'Дата: {datetime.strptime(str(datetime.now()), "%Y-%m-%d %H:%M:%S.%f")} | +: {money_log["add"]} | -: {money_log["remove"]}\n')
+        root.destroy()
+
+    
+    root = tk.Tk()
+    root.title('Подсчет процентов')
+    root.geometry(f'300x300+{int(root.winfo_screenwidth() / 2) - int(300 / 2)}+{int(root.winfo_screenheight() / 2) - int(300 / 2)}')
+
+    money = 0.0
+    input_money = 0.0
+    weeks = 0
+    money_log = {'add': 0.0, 'remove': 0.0}
     date = datetime.now()
     date = datetime(year=date.year, month=date.month, day=date.day)
+    date_start = date
 
     json_file = {}
     try:
@@ -33,33 +105,9 @@ def window():
         json_file = {'date_start': f'{date.year}.{date.month}.{date.day}',
                      'date': f'{date.year}.{date.month}.{date.day}',
                      'money': money}
-    
-    def entryEdited(event):
-        try:
-            nonlocal money
-            money = float(int(event.get()))
-            label['text'] = f'Ввод: {money}'
-        except ValueError:
-            label['text'] = 'Ввод:'
-
-    
-    def save(json_file: dict) -> None:
-        nonlocal money
-        money += json_file['money']
-        date_start = datetime(*map(int, json_file['date_start'].split('.')))
-        weeks = relativedelta(date, date_start)
-        weeks = weeks.days // 7
-        print(weeks)
-        if weeks > 0:
-            for _ in range(weeks):
-                money += (money * 0.1)
-            date_start += relativedelta(weeks=+weeks)
-            json_file['date_start'] = f'{date_start.year}.{date_start.month}.{date_start.day}'
-        json_file['money'] = money
-        json_file['date'] = f'{date.year}.{date.month}.{date.day}'
-        save_json(json_file)
-        print(json_file)
-
+    date_start = datetime(*map(int, json_file['date_start'].split('.')))
+    calc_weeks(date_start, date)
+         
 
     sv = StringVar()
     sv.trace('w', lambda name, index, mode, sv=sv: entryEdited(sv))
@@ -67,16 +115,30 @@ def window():
     entry = tk.Entry(root, textvariable=sv)
     entry.pack()
 
-    ok_btn = ttk.Button(command=lambda: save(json_file))
-    ok_btn['text'] = 'Сохранить'
-    ok_btn.pack()
+    save_btn = ttk.Button(command=lambda: save(json_file, calc_money))
+    save_btn['text'] = 'Сохранить'
+    save_btn.pack()
+
+    remove_btn = ttk.Button(command=lambda: save(json_file, remove))
+    remove_btn['text'] = 'Вычесть '
+    remove_btn.pack()
 
     label = tk.Label()
+    label['text'] = 'Ввод:'
     label.pack()
 
     label_from_json = tk.Label()
     label_from_json['text'] = f"Из файла: {json_file['money']}"
     label_from_json.pack()
+
+    label_calc_perc = tk.Label()
+    label_calc_perc['text'] = ''
+    label_calc_perc.pack()
+
+    root.protocol('WM_DELETE_WINDOW', report)
+
+
+    calc_percent(json_file, date_start)
 
     root.mainloop()
 
